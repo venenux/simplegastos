e9ae4ce100fb179f5aa62f94e7fc054c2bd24fb6
PICCORO Lenz McKAY <mckaygerhard@gmail.com>
2016-09-28 03:01:31 +0000

    * estructura de la DB creada y documentacion iniciada * tarea #433 iniciada y avanzada, manan finalizaria, para comenzar los querys * documentacion [[Sysgastosf0dbestructura]] *pendiente*

--- sysgastosweb/simplegastoswebphp/desarrollobasededatos/gastossystema-scrip-20160927.sql
+++ sysgastosweb/simplegastoswebphp/desarrollobasededatos/gastossystema-scrip-20160927.sql
@@ -9,18 +9,19 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 DROP TABLE IF EXISTS `registro_gastos` ;
 
 CREATE  TABLE IF NOT EXISTS `registro_gastos` (
-  `cod_registro` TEXT NOT NULL COMMENT 'era id_unico_autogenerado, es generado manual asi YYYYMMDDhhmmss' ,
-  `cod_sucursal` VARCHAR(40) NULL COMMENT 'esto es el sello de la tabla sucursal mas abajo',
-  `cod_categoria` VARCHAR(40) NULL ,
-  `cod_subcategoria` VARCHAR(40) NULL ,
-  `des_registro` VARCHAR(40) NOT NULL COMMENT 'esto era descripcion_gasto' ,
-  `mon_registro` DECIMAL NOT NULL ,
-  `fecha_registro` VARCHAR(40) NULL ,
-  `fecha_factura` VARCHAR(40) NULL ,
-  `sessionflag` VARCHAR(40) NULL COMMENT 'esto es quien_registro' ,
+  `cod_registro` TEXT NOT NULL COMMENT 'usa fecha YYYYMMDDhhmmss era id_unico_autogenerado' ,
+  `cod_sucursal` VARCHAR(40) NULL COMMENT 'sello de la entidad al cual se le adjudica' ,
+  `cod_categoria` VARCHAR(40) NULL COMMENT 'a que categoria de gasto' ,
+  `cod_subcategoria` VARCHAR(40) NULL COMMENT 'cual subcategoria' ,
+  `des_registro` VARCHAR(40) NOT NULL COMMENT 'cual fue el gasto esto era descripcion_gasto' ,
+  `mon_registro` DECIMAL NOT NULL COMMENT 'cuanto se gasto' ,
+  `fecha_registro` VARCHAR(40) NULL COMMENT 'YYYYMMDD innecesario, se deja por compatibilidad' ,
+  `fecha_factura` VARCHAR(40) NULL COMMENT 'YYYYMMDD de la factura si tiene' ,
+  `sessionflag` VARCHAR(40) NULL COMMENT 'esto es quien_registro YYYYMMDDhhmmss + cod_sucursal + . + ficha' ,
   `estado` VARCHAR(40) NULL ,
   `num_factura` VARCHAR(40) NULL COMMENT 'mumero de factura opcinal' )
-ENGINE = InnoDB;
+ENGINE = InnoDB
+COMMENT = 'tabla donde se inserta cada monto de gasto y su descripcion (el detalle)';
 
 
 -- -----------------------------------------------------
@@ -29,11 +30,12 @@ ENGINE = InnoDB;
 DROP TABLE IF EXISTS `categoria` ;
 
 CREATE  TABLE IF NOT EXISTS `categoria` (
-  `cod_categoria` TEXT NOT NULL ,
+  `cod_categoria` TEXT NOT NULL COMMENT 'YYYYMMDDhhmmss' ,
   `des_categoria` VARCHAR(40) NULL COMMENT 'descripcion_categoria' ,
-  `fecha_categoria` VARCHAR(40) NULL ,
-  `sessionflag` VARCHAR(40) NULL )
-ENGINE = InnoDB;
+  `fecha_categoria` VARCHAR(40) NULL COMMENT 'innecesario, por conpatibilidad' ,
+  `sessionflag` VARCHAR(40) NULL COMMENT 'esto es quien_registro YYYYMMDDhhmmss + cod_sucursal + . + ficha' )
+ENGINE = InnoDB
+COMMENT = 'tabla que representa los titulos de la matrix, el tipo general de gasto';
 
 
 -- -----------------------------------------------------
@@ -42,10 +44,10 @@ ENGINE = InnoDB;
 DROP TABLE IF EXISTS `subcategoria` ;
 
 CREATE  TABLE IF NOT EXISTS `subcategoria` (
-  `cod_subcategoria` INT(11) NOT NULL ,
-  `des_subcategoria` VARCHAR(40) NULL ,
-  `fecha_subcategoria` VARCHAR(40) NULL ,
-  `sessionflag` VARCHAR(40) NULL )
+  `cod_subcategoria` VARCHAR(40) NOT NULL COMMENT 'YYYYMMDDhhmmss' ,
+  `des_subcategoria` VARCHAR(40) NULL COMMENT 'que tipo de gasto en la categoria' ,
+  `fecha_subcategoria` VARCHAR(40) NULL COMMENT 'innecesario, por compatibilidad' ,
+  `sessionflag` VARCHAR(40) NULL COMMENT 'esto es quien_registro YYYYMMDDhhmmss + cod_sucursal + . + ficha' )
 ENGINE = InnoDB;
 
 
@@ -55,12 +57,14 @@ ENGINE = InnoDB;
 DROP TABLE IF EXISTS `registro_adjunto` ;
 
 CREATE  TABLE IF NOT EXISTS `registro_adjunto` (
-  `cod_adjunto` TEXT NOT NULL ,
-  `cod_registro` VARCHAR(40) NULL ,
-  `hex_adjunto` VARCHAR(40000) NULL ,
-  `fecha_adjunto` VARCHAR(40) NULL ,
-  `sessionflag` VARCHAR(40) NULL )
-ENGINE = InnoDB;
+  `cod_adjunto` TEXT NOT NULL COMMENT 'YYYYMMDDhhmmss' ,
+  `cod_registro` VARCHAR(40) NULL COMMENT 'a cual registro de gasto le pertenece este adjunto' ,
+  `hex_adjunto` VARCHAR(400000) NULL COMMENT 'la subida en base 64 del adjunto' ,
+  `ruta_adjunto` VARCHAR(40) NULL COMMENT 'ruta opcinal del archivo si esta en el sistema de ficheros' ,
+  `fecha_adjunto` VARCHAR(40) NULL COMMENT 'cuando se altero este adjunto' ,
+  `sessionflag` VARCHAR(40) NULL COMMENT 'esto es quien_registro YYYYMMDDhhmmss + cod_sucursal + . + ficha' )
+ENGINE = InnoDB
+COMMENT = 'tabla de escaneados de cada registro, un registro puede tener varios escaneos de facturas';
 
 
 -- -----------------------------------------------------
@@ -69,18 +73,19 @@ ENGINE = InnoDB;
 DROP TABLE IF EXISTS `usuarios` ;
 
 CREATE  TABLE IF NOT EXISTS `usuarios` (
-  `ficha` INT(11) NOT NULL COMMENT 'cod_usuario' ,
-  `intranet` VARCHAR(40) NULL ,
+  `ficha` VARCHAR(40) NOT NULL COMMENT 'cod_usuario, cedula en vnzla' ,
+  `intranet` VARCHAR(40) NULL COMMENT 'login del usuario, id del correo' ,
   `clave` VARCHAR(40) NULL ,
-  `nombre` VARCHAR(40) NULL ,
-  `estado` VARCHAR(40) NULL ,
-  `sessionflag` VARCHAR(40) NULL ,
-  `acc_lectura` VARCHAR(40) NULL ,
-  `acc_escribe` VARCHAR(40) NULL ,
-  `acc_modifi` VARCHAR(40) NULL ,
+  `nombre` VARCHAR(40) NULL COMMENT 'nombre y apellido' ,
+  `estado` VARCHAR(40) NULL COMMENT 'ACTIVO INACTIVO SUSPENDIDO INVALIDO' ,
+  `sessionflag` VARCHAR(40) NULL COMMENT 'esto es quien_registro YYYYMMDDhhmmss + cod_sucursal + . + ficha' ,
+  `acc_lectura` VARCHAR(40) NULL COMMENT 'modulos o pagina controlador que puede leer separados por barra' ,
+  `acc_escribe` VARCHAR(40) NULL COMMENT 'modulos o nombre controlador que puede crear registros separados por barra' ,
+  `acc_modifi` VARCHAR(40) NULL COMMENT 'modulos o nombre controlador que puede alterar separados por barra' ,
   `fecha_ficha` VARCHAR(40) NULL ,
-  `fecha_ultimavez` VARCHAR(40) NULL )
-ENGINE = InnoDB;
+  `fecha_ultimavez` VARCHAR(40) NULL COMMENT 'cuando fue la ultima vez que entro sesion' )
+ENGINE = InnoDB
+COMMENT = 'tabla de usuarios, el indicador applicacion permite saber si puede entrar en el app';
 
 
 -- -----------------------------------------------------
@@ -89,10 +94,11 @@ ENGINE = InnoDB;
 DROP TABLE IF EXISTS `log` ;
 
 CREATE  TABLE IF NOT EXISTS `log` (
-  `cod_log` TEXT NOT NULL COMMENT 'string con fecha y hora yyyymmddhhmmss' ,
-  `sessionflag` VARCHAR(40) NULL ,
-  `operacion` VARCHAR(40) NULL )
-ENGINE = InnoDB;
+  `cod_log` TEXT NOT NULL COMMENT 'yyyymmddhhmmss' ,
+  `sessionflag` VARCHAR(40) NULL COMMENT 'esto es quien_registro YYYYMMDDhhmmss + cod_sucursal + . + ficha' ,
+  `operacion` VARCHAR(40) NULL COMMENT 'en que modulo controlador y que realizo... y que tablas afecto' )
+ENGINE = InnoDB
+COMMENT = 'tabla de chismoso, cada operacion se graba aqui';
 
 
 -- -----------------------------------------------------
@@ -101,9 +107,10 @@ ENGINE = InnoDB;
 DROP TABLE IF EXISTS `sucursal_usuario` ;
 
 CREATE  TABLE IF NOT EXISTS `sucursal_usuario` (
-  `cod_usuario` INT(11) NOT NULL ,
-  `cod_sucursal` VARCHAR(40) NOT NULL )
-ENGINE = InnoDB;
+  `cod_usuario` VARCHAR(40) NOT NULL COMMENT 'login intranet de la tabla usuario' ,
+  `cod_sucursal` VARCHAR(40) NOT NULL COMMENT 'sello al cual esta asociado' )
+ENGINE = InnoDB
+COMMENT = 'relacion entre un usuario y en que sucursal puede adjudicar gastos';
 
 
 -- -----------------------------------------------------
@@ -112,11 +119,11 @@ ENGINE = InnoDB;
 DROP TABLE IF EXISTS `entidad` ;
 
 CREATE  TABLE IF NOT EXISTS `entidad` (
-  `cod_entidad` INT(11) NOT NULL ,
+  `cod_entidad` VARCHAR(40) NOT NULL COMMENT 'sello de la sucursal o entidad' ,
   `abr_entidad` VARCHAR(40) NULL COMMENT 'siglas sucursal' ,
   `des_entidad` VARCHAR(40) NULL COMMENT 'descripcion sucursal' )
 ENGINE = InnoDB
-COMMENT = 'es lo que era sucursales';
+COMMENT = 'representa las entidades que se le adjudican gastos es lo que era sucursales gastos';
 
 
 
