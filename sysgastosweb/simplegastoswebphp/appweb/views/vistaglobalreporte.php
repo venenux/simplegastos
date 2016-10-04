55edf44fad47a3e717abb68b5abc31a0accd72e0
PICCORO Lenz McKAY <mckaygerhard@gmail.com>
2016-10-04 06:34:08 +0000

    * eliminado archivos ya innecesarios heredados, dando paso a los nuevos controladores listos

--- sysgastosweb/simplegastoswebphp/appweb/views/vistaglobalreporte.php
+++
@@ -1,7 +0,0 @@
-<?php
-	$this->load->helper('html');
-		foreach($css_files as $file)
-		{	echo '<link type="text/css" rel="stylesheet" href="'.$file.'" />';	}
-		foreach($js_files as $file)
-		{	echo '<script src="'.$file.'"></script>';	}
-	echo $output;
