4b483009f30af3c42503e27f6dc2f389c2abf2bb
PICCORO Lenz McKAY <mckaygerhard@gmail.com>
2016-10-11 21:34:08 +0000

    * trabajada tarea #449 correcciones errores de codigo php

--- sysgastosweb/simplegastoswebphp/appweb/controllers/mimatrixcontroller.php
+++ sysgastosweb/simplegastoswebphp/appweb/controllers/mimatrixcontroller.php
@@ -1,7 +1,7 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class mimatrixcontroller extends CI_Controller {
-	
+
 	private $DBGASTO = null;
 	private $usuariologin, $sessionflag, $usuariocodger, $acc_lectura, $acc_escribe, $acc_modifi;
 
@@ -25,80 +25,13 @@ class mimatrixcontroller extends CI_Controller {
 	}
 
 	/**
-	 * Index Page cuando se invoca la url de este controlador, 
+	 * Index Page cuando se invoca la url de este controlador,
 	 * aqui se invoca la vista o otro metodo que la invoque
 	 * map to /index.php/plantillamatrixcontroler/index
 	 */
 	public function index()
 	{
-		$this->seccionformulario();
-	}
-	
-	public function seccionformulario()
-	{
-		/* ***** ini manejo de sesion ******************* */
-		$this->_verificarsesion();
-		$userdata = $this->session->all_userdata();
-		$usercorreo = $userdata['correo'];
-		$userintranet = $userdata['intranet'];
-		$sessionflag = $this->session->userdata('username').date("YmdHis");
-		$data['usercorreo'] = $usercorreo;
-		$data['userintranet'] = $userintranet;
-		$data['menu'] = $this->menu->general_menu();
-		/* ***** fin manejo de sesion ******************* */
-		
-		
-		/* ****** ini cargar y listaar CATEGORIAS para comboboxes u otros ********** */
-		$sqlcategoria = " select 
-		 ifnull(cod_categoria,'99999999999999') as cod_categoria,
-		 ifnull(des_categoria,'sin_descripcion') as des_categoria,
-		 ifnull(fecha_categoria, '20160101') as fecha_categoria
-		from categoria where ifnull(cod_categoria, '') <> '' and cod_categoria <> ''"; // documentado en wiki tabla y select
-		$resultadoscategoria = $this->db->query($sqlcategoria);
-		$arreglocategoriaes = array(''=>'');  // declaro un arreglo y lo lleno con la lista (sera el combobox)
-		foreach ($resultadoscategoria->result() as $row)
-			$arreglocategoriaes[''.$row->cod_categoria] = '' . $row->des_categoria . '-' . $row->fecha_categoria;
-		$data['list_categoria'] = $arreglocategoriaes; // meto en data para enviar a la vista
-		unset($arreglocategoriaes['']);
-		/* ****** fin cargar y listaar CATEGORIAS para comboboxes u otros ********** */
-
-		/* ****** ini cargar y listaar SUBCATEGORIAS para comboboxes vista ********** */
-		$sqlsubcategoria = " SELECT 
-		 ifnull(ca.cod_categoria,'0') as cod_categoria, ifnull(ca.des_categoria,'ninguna') as des_categoria, 
-		 ifnull(sb.cod_subcategoria,'0') as cod_subcategoria, ifnull(sb.des_subcategoria,'ninguna') as des_subcategoria,
-		 ca.fecha_categoria, sb.fecha_subcategoria, sb.sessionflag
-		FROM categoria AS ca JOIN subcategoria AS sb ON sb.cod_categoria = ca.cod_categoria 
-		WHERE ifnull(cod_subcategoria, '') <> '' AND cod_subcategoria <> ''"; // documentado en wiki tabla y select
-		$resultadossubcategoria = $this->db->query($sqlsubcategoria);
-		$arreglosubcategoriaes = array(''=>'');
-		foreach ($resultadossubcategoria->result() as $row)
-			$arreglosubcategoriaes[''.$row->cod_subcategoria] = $row->des_categoria . ' - ' . $row->des_subcategoria;
-		$data['list_subcategoria'] = $arreglosubcategoriaes; // agrega este arreglo una lista para el combo box
-		unset($arreglosubcategoriaes['']);
-		/* cargar y listaar las UBIUCACIONES que se usaran para registros */
-		$sqlentidad = "
-		select
-		 abr_entidad, abr_zona, des_entidad,
-		 ifnull(cod_entidad,'99999999999999') as cod_entidad,      -- YYYYMMDDhhmmss
-		 ifnull(des_entidad,'sin_descripcion') as des_entidad
-		from entidad
-		  where ifnull(cod_entidad, '') <> '' and cod_entidad <> ''
-		";
-		$resultadosentidad = $this->query($sqlentidad);
-		$arregloentidades = array(''=>'');
-		foreach ($resultadosentidad->result() as $row)
-		{
-			$arregloentidades[''.$row->cod_entidad] = $row->cod_entidad . ' - ' . $row->abr_entidad .' - ' . $row->des_entidad . ' ('. $row->abr_zona .')';
-		}
-		$data['list_entidad'] = $arregloentidades; // agrega este arreglo una lista para el combo box
-		unset($arregloentidades['']);
-		/* ****** fin cargar y listaar SUBCATEGORIAS para comboboxes vista ********** */
-		
-		/* ****** ini cargar y preparar para llamar y pintar vista ********** */
-		$this->load->view('header.php',$data);
-		$this->load->view('plantillamatrixvista.php',$data);
-		$this->load->view('footer.php',$data);
-		/* ****** fin cargar y preparar para llamar y pintar vista ********** */
+		$this->secciontablamatrix();
 	}
 
 	public function secciontablamatrix()
@@ -112,20 +45,18 @@ class mimatrixcontroller extends CI_Controller {
 		$data['usercorreo'] = $usercorreo;
 		$data['userintranet'] = $userintranet;
 		$data['menu'] = $this->menu->general_menu();
-		
+
 		/* ***** fin ********** */
-		
+
 		/* ******** inicio de los querys ************* */
 		$this->load->helper(array('form', 'url','inflector'));
-		
-		//preguntar cuantas tiendas sonm pra saber cuantas filas hay que recorrer
-		
+
 		 $queryentidades ="
-		 select 
+		 select
 		 cod_entidad as codigo, des_entidad as tiendas
 		 from
-		   entidad  
-		  where cod_entidad >=000 and   cod_entidad <=005
+		   entidad
+		  where (cod_entidad >=000 and   cod_entidad <=005) or (cod_entidad >=400 and   cod_entidad <=998)
 		 ";
 		$indicet=0;
 		$lastiendas= $this->db->query($queryentidades);
@@ -135,97 +66,97 @@ class mimatrixcontroller extends CI_Controller {
 			$tiendas[$row->codigo]=$row->tiendas;
 		}
 		$xtiendas= count($tiendas);// ya se cuantas tiendas
-		
-		
+
+
 		// crear el query sql para cargar las cabeceras
 		$querycabeceras ="
 		SELECT
-		   cod_categoria as codex,des_categoria  as categoria             
+		   cod_categoria as codex,des_categoria  as categoria
 		FROM
 		   categoria
 		";
 		// aquiio se establece las cabeceras
 		$micabeceras = $this->db->query($querycabeceras);
-		
-	
+
+
 		$categorias= array('0'=>'Tiendas');
 		foreach ($micabeceras->result() as $row)
 		{
 			$categorias[$row->codex]=$row->categoria;
-			
+
 		}
 		$categorias[count($categorias)]='   TOTAL:   ';
-		
+
 		/* ***** ini OBTENER DATOS DE FORMULARIO (con esto no me meto todavia) ***************************** */
 		$fechafiltramatrix = $this->input->get_post('fechafiltramatrix');
 		$cod_entidad = $this->input->get_post('cod_entidad');
 		$cod_subcategoria = $this->input->get_post('cod_subcategoria');
 		/* ***** fin OBTENER DATOS DE FORMULARIO ***************************** */
 		 // generar fila por fila la tabla
-		$maxcat=count($categorias) ; 
+		$maxcat=count($categorias) ;
 		$elgraantootal =0;
 		$filafinal=array();
 		$finalindex=1;
 		$switch=0;
 		foreach($tiendas as $indicetienda => $tiend)
 		{
-			$icat=0; 
-			
+			$icat=0;
+
 			$fila =array($tiend);
 			foreach ($categorias as $indicecategoria=> $descripcioncat)
 			{
 				$querysuma1="
-				Select  
+				Select
 				  ifnull(cast(sum(mon_registro) as decimal(30,2)),0) as suma
-				from 
-				  registro_gastos 
-				where 
+				from
+				  registro_gastos
+				where
 				registro_gastos.cod_entidad =  '".$indicetienda."'
-				   and 
+				   and
 				 registro_gastos.cod_categoria='".$indicecategoria."'";
                 //aqui se calcula el gasto categoria por tienda
                  if ($icat<$maxcat-1){
-					  	 
+
 				  if ($icat>0){
 				  $lasuma=$this->db->query($querysuma1);
                   foreach ($lasuma->result() as $row)
-		          { $total=$row->suma;break;}		
+		          { $total=$row->suma;break;}
 			      $fila[$icat]=$total;
-			      
+
 			      if ($switch==0)
 					   {// calculo de la suma de una categoria en todas las tiendas
 						   $querycatefulltiendas ="
-						   Select  
+						   Select
 							ifnull(cast(sum(mon_registro) as decimal(30,2)),0) as sumatienda
-						   from registro_gastos 
+						   from registro_gastos
 							 where registro_gastos.cod_categoria = '".$indicecategoria."'
 							 ";
 							$totalcat=$this->db->query($querycatefulltiendas);
 							  foreach ($totalcat->result() as $row)
-							   { $totaltiendasxcat=$row->sumatienda;break;}	
+							   { $totaltiendasxcat=$row->sumatienda;break;}
 							$filafinal[$finalindex]= $totaltiendasxcat;
 							$finalindex=  $finalindex+1;
-							
-						}	
-			      
-			      
+
+						}
+
+
 			      }$icat = $icat +1;
-			     }else 
+			     }else
 			     {
 			      // aqui se calcula el total en una categoria en una tienda
 			       $querygastotiendasfullcat="
-			      Select  
+			      Select
 			        ifnull(cast(sum(mon_registro)  as decimal(30,2)),0) as sumatienda
-			      from registro_gastos where 
+			      from registro_gastos where
 			        registro_gastos.cod_entidad = '".$indicetienda."'
 			      ";
-			  
+
 			      $totaltienda=$this->db->query($querygastotiendasfullcat);
 			      foreach ($totaltienda->result() as $row)
-		          { $totalfullcat=$row->sumatienda;break;}	
+		          { $totalfullcat=$row->sumatienda;break;}
 			        $fila[$icat]= $totalfullcat;
 			        //acumular el total cada categoria
-			        
+
 			      }
 			 }
 			   $this->table->add_row($fila);
