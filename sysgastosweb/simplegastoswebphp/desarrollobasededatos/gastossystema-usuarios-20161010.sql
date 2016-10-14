b79fe9f1ab313f68961029838b2a4e430fb189cd
PICCORO Lenz McKAY <mckaygerhard@gmail.com>
2016-10-14 03:55:04 +0000

    * trabajada tarea #456 cambios para campos nuevos en db * trabajada tarea #457 cambios logica y descripcion para permisos * nuevo campos de tipo de gasto, numero rif factura * campos no nulos ahora permiten nulos, evita errores de creacion en crud

--- sysgastosweb/simplegastoswebphp/desarrollobasededatos/gastossystema-usuarios-20161010.sql
+++ sysgastosweb/simplegastoswebphp/desarrollobasededatos/gastossystema-usuarios-20161010.sql
@@ -1,6 +1,2 @@
-INSERT INTO `gastossystema`.`usuarios` (`intranet`, `sello`, `nombre`, `cod_fondo`, `estado`) VALUES ('delgado_jesus', '34', 'Delgado Jesus', '', 'ACTIVO');
-INSERT INTO `gastossystema`.`usuarios` (`intranet`, `sello`, `nombre`, `estado`) VALUES ('jimenes_liliana', '34', 'Jimenes Liliana', 'ACTIVO');
-INSERT INTO `gastossystema`.`usuarios` (`intranet`, `sello`, `nombre`, `estado`) VALUES ('ortega_yamile',  '34', 'Ortega Santa Yamile', 'ACTIVO');
-INSERT INTO `gastossystema`.`usuarios` (`intranet`, `sello`, `nombre`, `estado`) VALUES ('polanco_ingrid', '34', 'Polanco Ingrid', 'ACTIVO');
-INSERT INTO `gastossystema`.`usuarios` (`intranet`, `sello`, `nombre`, `estado`) VALUES ('alvarez_sarahi', '34', 'Sarahi', 'ACTIVO');
-INSERT INTO `gastossystema`.`usuarios` (`ficha`, `intranet`, `clave`, `sello`, `nombre`, `estado`, `acc_lectura`, `acc_escribe`, `acc_modifi`, `sessionficha`) VALUES ('', 'usuario3', 'clave', '99', 'Usuario nuevo', 'ACTIVO', 'NADA', 'NADA', 'NADA', 'systema');
+
+INSERT INTO `gastossystema`.`entidad` (`cod_entidad`, `abr_zona`, `des_entidad`) VALUES ('998', 'CAP', 'Administracion systema');
