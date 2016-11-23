ed4f3aaa0114fe7465e0c05270c5ac8d0a57bcfc
PICCORO Lenz McKAY <mckaygerhard@gmail.com>
2016-11-23 22:05:00 +0000

    * inicio de sopote multiuser clase modelo inicial

--- sysgastosweb/simplegastoswebphp/appweb/models/manejousuarios.php
+++ sysgastosweb/simplegastoswebphp/appweb/models/manejousuarios.php
@@ -1,28 +1,66 @@
-<?php
-class Manejousuarios extends CI_Model 
+<?php class Manejousuarios extends CI_Model 
 { 
-   protected $dbxmppusers = null;
-   
-   public function __construct() 
-   {
-      parent::__construct();
-      $this->dbxmppusers = $this->load->database('simplexmpp', true);
-   }
+	protected $dbxmppusers = null;
+	protected $dbgastousers = null;
+	protected $dbxmpperror = null;
+	protected $dbgastoerror = null;
 
-   public function usuario_ejabberd($nombre, $contrasena)
-   {
-	   /*
-	   $this->db->select('id, nombre');
-	   $this->db->from('usuarios');
-	   $this->db->where('nombre', $nombre);
-	   $this->db->where('contrasena', $contrasena);
-	   $consulta = $this->db->get();
-	   $resultado = $consulta->row();
-	   return $resultado;
-	   */
-	   $sqlusuario = "select username,\"password\" as clave from users where username='".$nombre."' and '".$contrasena."' ";
-	   $query = $this->dbxmppusers->query($sqlusuario);
-	   $objectquery = $query->result();
-	   return $objectquery;
-   }
+	$usuario
+
+	public function __construct() 
+	{
+		parent::__construct();
+
+		try
+		{
+			$this->dbxmppusers = $this->load->database('simplexmpp', true);
+		}
+		catch(Exception $e)
+		{
+			$dbxmpperror = $e->getMessage();
+		}
+
+		try
+		{
+			$this->dbgastousers = $this->load->database('gastossystema', true);
+		}
+		catch(Exception $e)
+		{
+			$dbgastoerror = $e->getMessage();
+		}
+	}
+
+	public function usuario_ejabberd($nombre, $contrasena)
+	{
+		$sqlusuario = "select username,\"password\" as clave from users where username='".$nombre."' and '".$contrasena."' ";
+		if 
+		$query = $this->dbxmppusers->query($sqlusuario);
+		$objectquery = $query->result();
+		return $objectquery;
+	}
+
+	function get_last_ten_entries()
+	{
+		$query = $this->db->get('entries', 10);
+		return $query->result();
+	}
+
+	function insert_entry()
+	{
+		$this->title   = $_POST['title']; // please read the below note
+		$this->content = $_POST['content'];
+		$this->date	= time();
+
+		$this->db->insert('entries', $this);
+	}
+
+	function update_entry()
+	{
+		$this->title   = $_POST['title'];
+		$this->content = $_POST['content'];
+		$this->date	= time();
+
+		$this->db->update('entries', $this, array('id' => $_POST['id']));
+	}
+
 }
