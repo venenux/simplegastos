3b15f25ac72e01715b2e90493c54f1d05fc2a434
PICCORO Lenz McKAY <mckaygerhard@gmail.com>
2017-01-16 19:17:00 +0000

    * en el header solo se carga dos hojas de estilo, default.css y bootstrap.css * eliminado estilo catalog por defecto

--- sysgastosweb/simplegastoswebphp/appweb/views/header.php
+++ sysgastosweb/simplegastoswebphp/appweb/views/header.php
@@ -7,26 +7,16 @@ $this->load->helper('html');
 		echo meta($meta);
 		echo '<meta name="viewport" content="width=device-width, initial-scale=1.0">';
 		echo link_tag($linkdefcss);
-		//echo link_tag($linkde2css);
 		echo link_tag($linkappcss);
 		echo script_tag($linkbrownavdecsjs);
 		echo script_tag($linkvalidaformsjs);
 		//echo script_tag($linktabberdetaljs);
 		echo script_tag($linkdatepickerugl);
-		//if( isset($output) )
-		//{
-			//foreach($css_files as $file)
-			//{
-				//echo '<link type="text/css" rel="stylesheet" href="'.$file.'" />';	}
-			//foreach($js_files as $file)
-			//{	echo '<script src="'.$file.'"></script>';	}
-		//}
 	echo '</head>', PHP_EOL;
 	?>
 	<body onload = 'checkAvailable()' >
 		<div class="menu ">
 			<center>
-				<!--<img src="<?php echo $giflogo ?>" alt="Logo VNX Codeigniter" width="880" height="24" />-->
 				<?=$menu.PHP_EOL?>
 			</center>
 		</div>
