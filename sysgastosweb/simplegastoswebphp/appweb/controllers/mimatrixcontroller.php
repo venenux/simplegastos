7c482e370fc3924bba995d014da2101d25a80d74
PICCORO Lenz McKAY <mckaygerhard@gmail.com>
2017-01-19 15:37:11 +0000

    * final documentado todo el proceso matrix, vista y controlador #485 * http://intranet1.net.ve/proyectos/proyectos/projects/sysgastos/wiki/Sysgastoswebf1-db-matrix-y-totalizadores

--- sysgastosweb/simplegastoswebphp/appweb/controllers/mimatrixcontroller.php
+++ sysgastosweb/simplegastoswebphp/appweb/controllers/mimatrixcontroller.php
@@ -5,6 +5,85 @@ class mimatrixcontroller extends CI_Controller {
 	private $DBGASTO = null;
 	private $usuariologin, $sessionflag, $usuariocodger, $acc_lectura, $acc_escribe, $acc_modifi;
 
+
+/*
+
+LEER :
+* 
+* http://10.10.34.20/proyectos/proyectos/projects/sysgastos/wiki/Sysgastoswebf1-db-matrix-y-totalizadores
+* http://intranet1.net.ve/proyectos/proyectos/projects/sysgastos/wiki/Sysgastoswebf1-db-matrix-y-totalizadores
+
+1) query base que da todos los totales de las tienda s por cada categoria presente
+2) query base que da los totales de cada una categoria en todas las tiendas
+3) query de totales de tiendas en todas sus categorias ews agrupando el pimero sin mostrar categorias
+
+
+------- 1 inicio query 1 -------------
+    select 
+        `cod_entidad` AS `cod_entidad`,
+        `des_entidad` AS `des_entidad`,
+        `cod_categoria` AS `cod_categoria`,
+        `des_categoria` AS `des_categoria`,
+        `tipo_categoria` AS `tipo_categoria`,
+        `mon_registro` AS `mon_registro`,
+        `fecha_concepto` AS `fecha_concepto`,
+        `fecha_registro` AS `fecha_registro`
+    from
+(
+select 
+        `a`.`cod_entidad` AS `cod_entidad`,
+        `b`.`des_entidad` AS `des_entidad`,
+        `a`.`cod_categoria` AS `cod_categoria`,
+        `c`.`des_categoria` AS `des_categoria`,
+        ifnull(`c`.`tipo_categoria`, 'ADMINISTRATIVO') AS `tipo_categoria`,
+        sum(ifnull(`a`.`mon_registro`, 0)) AS `mon_registro`,
+        substr(`a`.`fecha_concepto`, 1, 6) AS `fecha_concepto`,
+        `a`.`fecha_registro` AS `fecha_registro`
+    from
+        ((`registro_gastos` `a`
+        left join `entidad` `b` ON ((`a`.`cod_entidad` = `b`.`cod_entidad`)))
+        left join `categoria` `c` ON ((`a`.`cod_categoria` = `c`.`cod_categoria`)))
+		-- justo aqui se debe colcoar los filtros de fecha, esto traeta todo si no se hace
+	group by `a`.`cod_entidad` , `a`.`cod_categoria` 
+    union select 
+        `entidad`.`cod_entidad` AS `cod_entidad`,
+        `entidad`.`des_entidad` AS `des_entidad`,
+        `categoria`.`cod_categoria` AS `cod_categoria`,
+        `categoria`.`des_categoria` AS `des_categoria`,
+        ifnull(`categoria`.`tipo_categoria`,
+                'ADMINISTRATIVO') AS `tipo_categoria`,
+        0 AS `mon_registro`,
+        '' AS `fecha_concepto`,
+        '' AS `fecha_registro`
+    from
+        (`categoria`
+        join `entidad`)
+    group by `entidad`.`cod_entidad` , `categoria`.`cod_categoria`
+)
+ as       `querymatrixnuivel1`
+    group by `cod_entidad` , `cod_categoria`
+    order by `cod_entidad` , `cod_categoria`
+
+------- 1 fin query 1 -------------
+
+
+------- 2 inicio query 2 -------------
+
+select
+des_categoria, cod_categoria, sum(mon_registro)
+ from 
+(
+-- aqui consulta query 1 incluido el fuiltro, via php es facil usar la variable ya asignada
+) as pepe
+group by cod_categoria
+
+
+-------- 2 fin de query 2 --------------
+
+
+
+*/
+
 	function __construct()
 	{
 		parent::__construct();
