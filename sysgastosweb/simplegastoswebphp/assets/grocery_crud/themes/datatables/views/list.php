328fa0f4e0476187efac4b8d00241ebba568c977
PICCORO Lenz McKAY <mckaygerhard@gmail.com>
2017-02-07 20:35:37 +0000

    * disminuido el tamano de busqueda activa en las cabeceras, sino coloca demasiado ancho la celda

--- sysgastosweb/simplegastoswebphp/assets/grocery_crud/themes/datatables/views/list.php
+++ sysgastosweb/simplegastoswebphp/assets/grocery_crud/themes/datatables/views/list.php
@@ -28,15 +28,7 @@
 	<thead>
 		<tr>
 			<?php foreach($columns as $column){?>
-				<th><?php echo $column->display_as; ?></th>
-			<?php }?>
-			<?php if(!$unset_delete || !$unset_edit || !$unset_read || !empty($actions)){?>
-			<th class='actions'><?php echo $this->l('list_actions'); ?></th>
-			<?php }?>
-		</tr>
-		<tr>
-			<?php foreach($columns as $column){?>
-				<th><input type="text" name="<?php echo $column->field_name; ?>" placeholder="<?php echo $this->l('list_search').' '.$column->display_as; ?>" class="search_<?php echo $column->field_name; ?>" /></th>
+				<th><input type="text" size="7" name="<?php echo $column->field_name; ?>" placeholder="<?php echo $this->l('list_search').' '.$column->display_as; ?>" class="thead search_<?php echo $column->field_name; ?>" /></th>
 			<?php }?>
 			<?php if(!$unset_delete || !$unset_edit || !$unset_read || !empty($actions)){?>
 				<th>
@@ -48,6 +40,14 @@
 						<span class="ui-button-text"><?php echo $this->l('list_clear_filtering');?></span>
 					</a>
 				</th>
+			<?php }?>
+		</tr>
+		<tr>
+			<?php foreach($columns as $column){?>
+				<th><?php echo $column->display_as; ?></th>
+			<?php }?>
+			<?php if(!$unset_delete || !$unset_edit || !$unset_read || !empty($actions)){?>
+			<th class='actions'><?php echo $this->l('list_actions'); ?></th>
 			<?php }?>
 		</tr>
 	</thead>
