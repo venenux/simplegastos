863de207fb53be13e387c6da80be68cff84e9f01
PICCORO Lenz McKAY <mckaygerhard@gmail.com>
2017-02-09 15:13:39 +0000

    * anadido consulta de indicadores que genera la data (falta la de ventas) * NOTA: esta consulta sera ejecutada del lado de sybase, usando la proxytable y la de venta * respaldo de los indicadores actuales.

---
+++ sysgastosweb/simplegastoswebphp/desarrollobasededatos/gastossystema-select-total-tiendas-indicadores.sql
@@ -0,0 +1,19 @@
+SELECT 
+		CONVERT( CONVERT( DATE_FORMAT(NOW(),'%Y%m%d%H%i%s'), UNSIGNED) + CONVERT(a.cod_entidad,UNSIGNED), CHAR) as cod_indicador,
+        a.cod_entidad, 
+		-- b.des_entidad, 
+        IFNULL(SUM(IFNULL(a.mon_registro, 0)), 0) as mon_gastototal,
+		0 as mon_ventatotal,
+		SUBSTRING(a.fecha_concepto, 1, 6) as fecha_mes, 
+		'20160117998systemas', 
+		null
+    FROM
+        registro_gastos a
+    LEFT JOIN entidad b ON a.cod_entidad = b.cod_entidad
+    where
+        a.cod_registro <> ''
+            and b.status <> 'INACTIVO'
+            and CONVERT( a.fecha_concepto , UNSIGNED) >= CONVERT( '20170100' , UNSIGNED)
+            and CONVERT( a.fecha_concepto , UNSIGNED) <= CONVERT( '20170190' , UNSIGNED)
+    group by cod_entidad
+    order by b.des_entidad
