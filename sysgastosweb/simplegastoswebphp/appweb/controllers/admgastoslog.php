530bc00b177d591b36ba2122e9192310e504401a
PICCORO Lenz McKAY <mckaygerhard@gmail.com>
2017-04-15 16:16:13 +0000

    * controlador de registros de actividad activado en menu y funcionando * controlador de logs y vista de logs con buscador por fecha y por usuario que lo realizo

--- sysgastosweb/simplegastoswebphp/appweb/controllers/admgastoslog.php
+++ sysgastosweb/simplegastoswebphp/appweb/controllers/admgastoslog.php
@@ -24,6 +24,27 @@ class admgastoslog extends CI_Controller {
 	{
 		if( $this->session->userdata('logueado') != TRUE)
 			redirect('manejousuarios/desverificarintranet');
+		$usuariocodgernow = $this->session->userdata('cod_entidad');
+		if( is_array($usuariocodgernow) )
+		{
+			if (in_array("998", $usuariocodgernow) or in_array("1000", $usuariocodgernow) )
+				$this->nivel = 'administrador';
+			else if (in_array("163", $usuariocodgernow) or in_array("251", $usuariocodgernow) )
+				$this->nivel = 'contabilidad';
+			else
+				$this->nivel = 'especial';
+		}
+		else
+		{
+			if( $usuariocodgernow == '998' or $usuariocodgernow == '1000' )
+				$this->nivel = 'administrador';
+			else if( ( $usuariocodgernow > 399 and $usuariocodgernow < 998) or $usuariocodgernow == '196' or $usuariocodgernow == '252' or $usuariocodgernow == '200' )
+				$this->nivel = 'sucursal';
+			else if ( $usuariocodgernow == '163' or $usuariocodgernow == '251' )
+				$this->nivel = 'contabilidad';
+			else
+				$this->nivel = 'especial';
+		}
 	}
 
 	/**
@@ -33,10 +54,21 @@ class admgastoslog extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->seccionlog();
+		$this->seccionlogpre();
 	}
 
-	public function seccionlog()
+	public function seccionlogpre()
+	{
+		$this->_verificarsesion();
+		$data['menu'] = $this->menu->menudesktop();
+		$data['accionejecutado'] = 'seccionlogpre';	// para cargar parte especifica de la vista envio un parametro accion
+		$data['accionejecutara'] = 'seccionlogver';	// para cargar parte especifica de la vista envio un parametro accion
+		$this->load->view('header.php',$data);
+		$this->load->view('admgastoslog.php',$data);
+		$this->load->view('footer.php',$data);
+	}
+
+	public function seccionlogver()
 	{
 		/* ***** ini manejo de sesion ******************* */
 		$this->_verificarsesion();
@@ -55,9 +87,39 @@ class admgastoslog extends CI_Controller {
 		$this->config->load('grocery_crud');
 		$this->config->set_item('grocery_crud_dialog_forms',true);
 		$this->config->set_item('grocery_crud_default_per_page',80);
+		
+		$usuariocodgernow = $this->session->userdata('cod_entidad');
+		$userintran = $this->session->userdata('intranet');
+		$fec_registroini = $this->input->get_post('fec_registroini');
+		$fec_registrofin = $this->input->get_post('fec_registrofin');
+		$sessionfichav = $this->input->get_post('sessionficha');
+
+		// ******* ini nombres de tablas para filtrar los datos:
+		$segurodelatabla = rand(6,8);
+		$tablalogorigin = "log";
+		$tablalogsegura = "log_".$userintran . $segurodelatabla;
+
+		$sqltablalogs = "
+			CREATE TABLE IF NOT EXISTS `".$tablalogsegura."` SELECT * FROM (`".$tablalogorigin."`)
+			WHERE ( cod_log <> '' or sessionficha <> '') ";
+				if ( $this->nivel != 'administrador' ) 	$sqltablalogs .= "AND sessionficha LIKE '%".$userintran."%' ";
+				if ( trim($fec_registroini) != '')	$sqltablalogs .= " AND CONVERT(substring(cod_log,1,8),UNSIGNED) >= ".$this->db->escape_str($fec_registroini)." ";
+				if ( trim($fec_registrofin) != '')	$sqltablalogs .= " AND CONVERT(substring(cod_log,1,8),UNSIGNED) <= ".$this->db->escape_str($fec_registrofin)." ";
+				if ( $sessionfichav != '')	$sqltablagastousr .= " AND (sessionficha LIKE '".str_replace('.','_',$this->db->escape_str($sessionfichav))."' or sessionflag LIKE '".str_replace('.','_',$this->db->escape_str($sessionfichav))."') ";
+		$sqltablalogs .= " ORDER BY cod_log DESC ";
+		$sqltablapre = "DROP TABLE IF EXISTS ".$tablalogsegura.";";
+		if ( $this->nivel != 'administrador')
+			$sqltablalogs .= " LIMIT 800";
+		else
+			$sqltablalogs .= " LIMIT 2000";
+		$this->db->query($sqltablapre);	// remuevo la viejas o datos viejos si hay aun
+		$this->db->query($sqltablalogs);		// recreo con el select la tabla temporal y se usara
+		$this->load->helper(array('inflector','url'));
+		$this->load->library('grocery_CRUD');
 		$crud = new grocery_CRUD();
-	    $crud->set_table('log');
-		$crud->set_theme('datatables'); // flexigrid tiene bugs en varias cosas
+		$crud->set_table($tablalogsegura);
+		$crud->set_primary_key('cod_log');
+		$crud->set_theme('bootstrap');
 		$crud->columns('cod_log','operacion','sessionficha');
 		$crud
 			 ->display_as('cod_log','Cuando')
@@ -66,10 +128,13 @@ class admgastoslog extends CI_Controller {
 		$crud->unset_add();
 		$crud->unset_edit();
 		$crud->unset_delete();
+		//$crud->unset_export();
 		$crud->field_type('operacion','text');
 		$this->load->helper(array('form', 'url','inflector'));
 		$output = $crud->render();
 		$data['menu'] = $this->menu->menudesktop();
+		$data['accionejecutado'] = 'seccionlogver';	// para cargar parte especifica de la vista envio un parametro accion
+		$data['accionejecutara'] = 'seccionlogpre';	// para cargar parte especifica de la vista envio un parametro accion
 		$this->load->view('header.php',$data);
 		$this->load->view('admgastoslog.php',$output);
 		$this->load->view('footer.php',$data);
--- sysgastosweb/simplegastoswebphp/appweb/views/admgastoslog.php
+++ sysgastosweb/simplegastoswebphp/appweb/views/admgastoslog.php
@@ -1,10 +1,75 @@
 	<br>
 	<?php
-		echo form_fieldset('Registros de actividad',array('class'=>'container_blue containerin')) . PHP_EOL;
-		foreach($css_files as $file)
-		{	echo '<link type="text/css" rel="stylesheet" href="'.$file.'" />';	}
-		foreach($js_files as $file)
-		{	echo '<script src="'.$file.'"></script>';	}
+	
+	$classinput = array('class'=>' form-input-box btn containerin');
+
+	if( !isset($mens) )
+		$mens = '<strong>Registros de actividades importantes</strong>';
+
+	//  cargar la session o aseguramiento que exista un objeto session
+	if( !isset($this->session) )
+		$usuariocodgernow = null;
+	else
+		$usuariocodgernow = $this->session->userdata('cod_entidad');
+
+	// que parte del formulario y a donde ira a viajar el html
+	if( !isset($accionejecutado) )
+		$accionejecutado = 'seccionlogpre';
+	if( !isset($accionejecutara) )
+		$accionejecutara = 'seccionlogver';
+
+	// cargar las ubicciones/centro costo o aseguramiento que exista
+	if( !isset($list_entidad) )
+		$list_entidad = array($usuariocodgernow => 'Los propios gastos');
+
+	// cargar las categorias y subcategorias o aseguramiento que exista
+	if( !isset($list_categoria) ) $list_categoria = array('' => 'N/A');
+	if( !isset($list_subcategoria) ) $list_subcategoria = array('' => 'Sin permiso para cargar');
+	if( !isset($cod_subcategoria) ) $cod_subcategoria = '';
+
+	$fec_registroini='';
+	$idfecdesde='fec_registroini';
+	$valoresinputfecha1ini = array('name'=>$idfecdesde,'id'=>$idfecdesde, 'onclick'=>'javascript:NewCssCal(\''.$idfecdesde.'\',\'yyyyMMdd\',\'arrow\')','readonly'=>'readonly','value'=>set_value($idfecdesde, $$idfecdesde));
+	$fec_registrofin='';
+	$idfechasta='fec_registrofin';
+	$valoresinputfecha1fin = array('name'=>$idfechasta,'id'=>$idfechasta, 'onclick'=>'javascript:NewCssCal(\''.$idfechasta.'\',\'yyyyMMdd\',\'arrow\')','readonly'=>'readonly','value'=>set_value($idfechasta, $$idfechasta));
+
+	$this->table->clear();
+	$tmplnewtable = array ( 'table_open'  => '<table border="0" cellpadding="0" cellspacing="0" class="table">' );
+	$this->table->set_template($tmplnewtable);
+
+	if ($accionejecutado == 'seccionlogpre')
+	{
+		echo br() . PHP_EOL;
+		echo form_fieldset('¿Que desea realizar?',array('class'=>'containerin')) . PHP_EOL;
+				$htmlformaattributos = array('name'=>'formularioordendespachogenerar','class'=>'formularios','onSubmit'=>'return validageneric(this);');
+		echo form_open_multipart('admgastoslog/seccionlogver', $htmlformaattributos) . PHP_EOL;
+		$this->table->clear();
+			$this->table->add_row('Del dia:',form_input($valoresinputfecha1ini).PHP_EOL.' al '.form_input($valoresinputfecha1fin).br().PHP_EOL);
+			$this->table->add_row('Por intranet:', form_input('sessioncarga','').br().PHP_EOL);
+		echo $this->table->generate();
+		echo form_hidden('accionejecutado',$accionejecutado).br().PHP_EOL;
+		echo form_hidden('accionejecutara','seccionlogver').br().PHP_EOL;
+		echo form_submit('gastofiltrarya', 'Ver actividad', 'class="btn-primary btn"');
+		echo form_close() . PHP_EOL;
+		echo form_fieldset_close() . PHP_EOL;
+		echo br().PHP_EOL;
+	}
+	else if ($accionejecutado == 'seccionlogver')
+	{
+		echo br().PHP_EOL;
+		echo form_fieldset('Actividades registradas de importancia </strong> SOLO LOS ULTIMOS REGISTROS!!!',array('class'=>'containerin ')) . PHP_EOL;
+		echo form_hidden('accionejecutado',$accionejecutado).br().PHP_EOL;
+		echo form_hidden('accionejecutara','seccionlogpre').br().PHP_EOL;
+		$buttonnuevabusqueda = form_submit('gastofiltrarya', 'Nueva busqueda', 'class="btn-primary btn"');
+		echo anchor('admgastoslog', $buttonnuevabusqueda);
+		if( isset($css_files) )
+			foreach($css_files as $file)
+			{	echo '<link type="text/css" rel="stylesheet" href="'.$file.'" />';	}
+		if( isset($js_files) )
+			foreach($js_files as $file)
+			{	echo '<script src="'.$file.'"></script>';	}
 		echo $output;
 		echo form_fieldset_close() . PHP_EOL;
+	}
 	?>
