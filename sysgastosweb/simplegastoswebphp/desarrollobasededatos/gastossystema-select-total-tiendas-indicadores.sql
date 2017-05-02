0ab142ecb178f21e922b964a4bfacd298a858e8f
PICCORO Lenz McKAY <mckaygerhard@gmail.com>
2017-05-02 21:04:40 +0000

    * ajuste de ocnsulta para indicadores, campos deben coincidir para mejor resultado

--- sysgastosweb/simplegastoswebphp/desarrollobasededatos/gastossystema-select-total-tiendas-indicadores.sql
+++ sysgastosweb/simplegastoswebphp/desarrollobasededatos/gastossystema-select-total-tiendas-indicadores.sql
@@ -5,15 +5,15 @@ SELECT
         IFNULL(SUM(IFNULL(a.mon_registro, 0)), 0) as mon_gastototal,
 		0 as mon_ventatotal,
 		SUBSTRING(a.fecha_concepto, 1, 6) as fecha_mes, 
-		'20160117998systemas', 
-		null
+		'20160117998systemas' as sessionficha, 
+		null as sessionflag
     FROM
         registro_gastos a
     LEFT JOIN entidad b ON a.cod_entidad = b.cod_entidad
     where
         a.cod_registro <> ''
             and b.status <> 'INACTIVO'
-            and CONVERT( a.fecha_concepto , UNSIGNED) >= CONVERT( '20170100' , UNSIGNED)
-            and CONVERT( a.fecha_concepto , UNSIGNED) <= CONVERT( '20170190' , UNSIGNED)
+            and CONVERT( a.fecha_concepto , UNSIGNED) >= CONVERT( '20170300' , UNSIGNED)
+            and CONVERT( a.fecha_concepto , UNSIGNED) <= CONVERT( '20170390' , UNSIGNED)
     group by cod_entidad
     order by b.des_entidad
